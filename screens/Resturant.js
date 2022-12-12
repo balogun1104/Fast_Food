02a009a0fc35a0c927937a00f9050b57e4db270d
Balogun Razak <razak2free@gmail.com>
2022-12-12 16:47:21 +0100

    Update Resturant.js

--- screens/Resturant.js
+++ screens/Resturant.js
@@ -16,7 +16,7 @@ const Resturant = ({route, navigation}) => {
 
     const renderHeader = () => {
          return (
-              <View style={{ flexDirection: 'row'}}>
+              <View style={{ flexDirection: 'row',  height: 50}}>
                 <TouchableOpacity style={{
                     width: 50,
                     paddingLeft: SIZES.padding2 * 2,
@@ -49,30 +49,125 @@ const Resturant = ({route, navigation}) => {
                     }}
                     >
                         <Text style={{
-                             ...FONTS.h3
+                             ...FONTS.h2
                         }}>{restaurant?.name}</Text>
                     </View>
                 </View>
-
-                 <TouchableOpacity style={{ 
+                <TouchableOpacity style={{ 
                     width: 50,
-                    paddingRight: SIZES.padding * 2,
+                    paddingRight: SIZES.padding2 * 3,
                     justifyContent: 'center',
                  }} />
                  <Image 
                  source={icons.list}
                  resizeMode='contain'
                  style={{
-                    height: 40,
+                    height: 30,
                     width: 30,
                  }}
                  />
               </View>
          )
     }
+
+    // FoodInfo
+
+    const renderFoodInfo = () => {
+        return (
+            <Animated.ScrollView
+            horizontal
+            pagingEnabled
+            scrollEventThrottle={16}
+            snapToAlignment='center'
+            showsHorizontalScrollIndicator={false}
+            >
+                {
+                     restaurant?.menu.map((item, index) => (
+                        <View 
+                        key={`menu-${index}`}
+                        style={{alignItems: 'center'}}
+                        >
+                            <View 
+                            style={{height: SIZES.height * 0.35}}
+                            >
+                                {/* FOod Image */}
+                                 <Image 
+                                 source={item.photo}
+                                 resizeMode= 'cover'
+                                 style={{
+                                    width: SIZES.width,
+                                    height: '100%'
+                                }}
+                                 />
+
+                                 {/* Quantity  Left*/}
+
+                                 <View style={{
+                                    position: 'absolute',
+                                    justifyContent: 'center',
+                                    width: SIZES.width,
+                                    height: 50,
+                                    bottom: -20,
+                                    flexDirection: 'row',
+                                 }}>
+                                     <TouchableOpacity
+                                     style={{
+                                        width: 50,
+                                        backgroundColor: COLORS.white,
+                                        alignItems: 'center',
+                                        justifyContent: 'center',
+                                        borderTopLeftRadius: 25,
+                                        borderBottomLeftRadius: 25
+                                     }}
+                                     >
+                                        <Text 
+                                        style={{
+                                            ...FONTS.body1
+                                        }}
+                                        >-</Text>
+                                     </TouchableOpacity>
+                                     {/* Quantity Itself */}
+                                     <View style={{
+                                        width: 50,
+                                        alignItems: 'center',
+                                        justifyContent: 'center',
+                                        backgroundColor: COLORS.white
+                                     }}>
+                                         <Text 
+                                         style={{
+                                            ...FONTS.h2
+                                         }}
+                                         >5</Text>
+                                     </View>
+                                     {/* Quantity Right */}
+                                      <TouchableOpacity
+                                      style={{
+                                        width: 50,
+                                        backgroundColor: COLORS.white,
+                                        alignItems: 'center',
+                                        justifyContent: 'center',
+                                        borderTopRightRadius: 25,
+                                        borderBottomRightRadius: 25
+                                    }}
+                                      >
+                                         <Text 
+                                        style={{
+                                            ...FONTS.body1
+                                        }}
+                                        >+</Text>
+                                      </TouchableOpacity>
+                                 </View>
+                            </View>
+                        </View>
+                     ))
+                }
+            </Animated.ScrollView>
+        )
+    }
   return (
     <SafeAreaView style= {styles.container}>
         {renderHeader()}
+        {renderFoodInfo()}
     </SafeAreaView>
   )
 }
