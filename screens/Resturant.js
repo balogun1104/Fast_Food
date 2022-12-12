059efc019dc4a57d80880e271c5e3dfa3891defb
Balogun Razak <razak2free@gmail.com>
2022-12-12 10:07:09 +0100

    Update Resturant.js

--- screens/Resturant.js
+++ screens/Resturant.js
@@ -1,289 +1,85 @@
+import { View, Text, StyleSheet, SafeAreaView, TouchableOpacity, Image, Animated } from 'react-native'
+import React, { useEffect, useState } from 'react'
+import { isIphoneX } from 'react-native-iphone-x-helper';
+import { icons, COLORS, SIZES, FONTS } from '../constants';
 
-import React, {useState} from 'react'
+const Resturant = ({route, navigation}) => {
+    const [restaurant, setRestaurant] =  useState(undefined);
+    const [currentLocation, setCurrentLocation] = useState(undefined);
 
-import {
-    StyleSheet,
-    SafeAreaView,
-    View,
-    Text,
-    TouchableOpacity,
-    Image,
-    Animated
-} from "react-native";
-import { isIphoneX } from 'react-native-iphone-x-helper'
-import { icons, COLORS, SIZES, FONTS } from '../constants'
+    useEffect(() => {
+        let {item, currentLocation}  = route.params;
+        
+        setRestaurant(item);
+        setCurrentLocation(currentLocation); 
+    }, []);
 
-const Resturant = ({ route, navigation }) => {
-
-  const scrollX = new Animated.Value(0);
-  const [restaurant, setRestaurant] = useState(null);
-  const [currentLocation, setCurrentLocation] = useState(null);
-
-
-
-  function renderHeader() {
-    return (
-        <View style={{ flexDirection: 'row' }}>
-            <TouchableOpacity
-                style={{
+    const renderHeader = () => {
+         return (
+              <View style={{ flexDirection: 'row'}}>
+                <TouchableOpacity style={{
                     width: 50,
-                    paddingLeft: SIZES.padding * 2,
-                    justifyContent: 'center'
+                    paddingLeft: SIZES.padding2 * 2,
+                    justifyContent: 'center',
                 }}
-                onPress={() => navigation.goBack()}
-            >
-                <Image
+                onPress={() => navigation.goBack() }
+                >
+                    <Image
                     source={icons.back}
-                    resizeMode="contain"
+                    resizeMode='contain'
                     style={{
                         width: 30,
-                        height: 30
+                        height: 30,
                     }}
-                />
-            </TouchableOpacity>
-
-            {/* Restaurant Name Section */}
-            <View
-                style={{
+                    />
+                </TouchableOpacity>
+                {/* Restuarnt Name Section */}
+                <View style={{
                     flex: 1,
+                    justifyContent: 'center',
                     alignItems: 'center',
-                    justifyContent: 'center'
-                }}
-            >
-                <View
-                    style={{
+                }}>
+                    <View style={{
                         height: 50,
-                        alignItems: 'center',
                         justifyContent: 'center',
+                        alignItems: 'center',
                         paddingHorizontal: SIZES.padding * 3,
                         borderRadius: SIZES.radius,
                         backgroundColor: COLORS.lightGray3
                     }}
-                >
-                    <Text style={{ ...FONTS.h3 }}>{restaurant?.name}</Text>
-                </View>
-            </View>
-
-            <TouchableOpacity
-                style={{
-                    width: 50,
-                    paddingRight: SIZES.padding * 2,
-                    justifyContent: 'center'
-                }}
-            >
-                <Image
-                    source={icons.list}
-                    resizeMode="contain"
-                    style={{
-                        width: 30,
-                        height: 30
-                    }}
-                />
-            </TouchableOpacity>
-        </View>
-
-        
-    )
-}
-
-
-  return (
-    <SafeAreaView style={styles.container}>
-    {renderHeader()}
-    {renderFoodInfo()}
-    </SafeAreaView>
-  )
-}
-
-   function renderHeader() {
-        return (
-            <View style={{ flexDirection: 'row' }}>
-                <TouchableOpacity
-                    style={{
-                        width: 50,
-                        paddingLeft: SIZES.padding * 2,
-                        justifyContent: 'center'
-                    }}
-                    onPress={() => navigation.goBack()}
-                >
-                    <Image
-                        source={icons.back}
-                        resizeMode="contain"
-                        style={{
-                            width: 30,
-                            height: 30
-                        }}
-                    />
-                </TouchableOpacity>
-
-                {/* Restaurant Name Section */}
-                <View
-                    style={{
-                        flex: 1,
-                        alignItems: 'center',
-                        justifyContent: 'center'
-                    }}
-                >
-                    <View
-                        style={{
-                            height: 50,
-                            alignItems: 'center',
-                            justifyContent: 'center',
-                            paddingHorizontal: SIZES.padding * 3,
-                            borderRadius: SIZES.radius,
-                            backgroundColor: COLORS.lightGray3
-                        }}
                     >
-                        <Text style={{ ...FONTS.h3 }}>{restaurant?.name}</Text>
+                        <Text style={{
+                             ...FONTS.h3
+                        }}>{restaurant?.name}</Text>
                     </View>
                 </View>
 
-                <TouchableOpacity
-                    style={{
-                        width: 50,
-                        paddingRight: SIZES.padding * 2,
-                        justifyContent: 'center'
-                    }}
-                >
-                    <Image
-                        source={icons.list}
-                        resizeMode="contain"
-                        style={{
-                            width: 30,
-                            height: 30
-                        }}
-                    />
-                </TouchableOpacity>
-            </View>
-        )
+                 <TouchableOpacity style={{ 
+                    width: 50,
+                    paddingRight: SIZES.padding * 2,
+                    justifyContent: 'center',
+                 }} />
+                 <Image 
+                 source={icons.list}
+                 resizeMode='contain'
+                 style={{
+                    height: 40,
+                    width: 30,
+                 }}
+                 />
+              </View>
+         )
     }
-
-    function renderFoodInfo() {
-        return (
-            <Animated.ScrollView
-                horizontal
-                pagingEnabled
-                scrollEventThrottle={16}
-                snapToAlignment="center"
-                showsHorizontalScrollIndicator={false}
-                onScroll={Animated.event([
-                    { nativeEvent: { contentOffset: { x: scrollX } } }
-                ], { useNativeDriver: false })}
-            >
-                {
-                    restaurant?.menu.map((item, index) => (
-                        <View
-                            key={`menu-${index}`}
-                            style={{ alignItems: 'center' }}
-                        >
-                            <View style={{ height: SIZES.height * 0.35 }}>
-                                {/* Food Image */}
-                                <Image
-                                    source={item.photo}
-                                    resizeMode="cover"
-                                    style={{
-                                        width: SIZES.width,
-                                        height: "100%"
-                                    }}
-                                />
-
-                                {/* Quantity */}
-                                <View
-                                    style={{
-                                        position: 'absolute',
-                                        bottom: - 20,
-                                        width: SIZES.width,
-                                        height: 50,
-                                        justifyContent: 'center',
-                                        flexDirection: 'row'
-                                    }}
-                                >
-                                    <TouchableOpacity
-                                        style={{
-                                            width: 50,
-                                            backgroundColor: COLORS.white,
-                                            alignItems: 'center',
-                                            justifyContent: 'center',
-                                            borderTopLeftRadius: 25,
-                                            borderBottomLeftRadius: 25
-                                        }}
-                                        onPress={() => editOrder("-", item.menuId, item.price)}
-                                    >
-                                        <Text style={{ ...FONTS.body1 }}>-</Text>
-                                    </TouchableOpacity>
-
-                                    <View
-                                        style={{
-                                            width: 50,
-                                            backgroundColor: COLORS.white,
-                                            alignItems: 'center',
-                                            justifyContent: 'center'
-                                        }}
-                                    >
-                                        <Text style={{ ...FONTS.h2 }}>{getOrderQty(item.menuId)}</Text>
-                                    </View>
-
-                                    <TouchableOpacity
-                                        style={{
-                                            width: 50,
-                                            backgroundColor: COLORS.white,
-                                            alignItems: 'center',
-                                            justifyContent: 'center',
-                                            borderTopRightRadius: 25,
-                                            borderBottomRightRadius: 25
-                                        }}
-                                        onPress={() => editOrder("+", item.menuId, item.price)}
-                                    >
-                                        <Text style={{ ...FONTS.body1 }}>+</Text>
-                                    </TouchableOpacity>
-                                </View>
-                            </View>
-
-                            {/* Name & Description */}
-                            <View
-                                style={{
-                                    width: SIZES.width,
-                                    alignItems: 'center',
-                                    marginTop: 15,
-                                    paddingHorizontal: SIZES.padding * 2
-                                }}
-                            >
-                                <Text style={{ marginVertical: 10, textAlign: 'center', ...FONTS.h2 }}>{item.name} - {item.price.toFixed(2)}</Text>
-                                <Text style={{ ...FONTS.body3 }}>{item.description}</Text>
-                            </View>
-
-                            {/* Calories */}
-                            <View
-                                style={{
-                                    flexDirection: 'row',
-                                    marginTop: 10
-                                }}
-                            >
-                                <Image
-                                    source={icons.fire}
-                                    style={{
-                                        width: 20,
-                                        height: 20,
-                                        marginRight: 10
-                                    }}
-                                />
-
-                                <Text style={{
-                                    ...FONTS.body3, color: COLORS.darygray
-                                }}>{item.calories.toFixed(2)} cal</Text>
-                            </View>
-                        </View>
-                    ))
-                }
-            </Animated.ScrollView>
-        )
-    }
-
+  return (
+    <SafeAreaView style= {styles.container}>
+        {renderHeader()}
+    </SafeAreaView>
+  )
+}
 const styles = StyleSheet.create({
-  container: {
-      flex: 1,
-      backgroundColor: COLORS.lightGray2
-  }
+    container: {
+        flex: 1,
+        backgroundColor: COLORS.lightGray2
+    }
 })
-
 export default Resturant
