3b8b92d3ce96bd2c2d2e2a5fb2cc0d6d4c1202ca
Balogun Razak <razak2free@gmail.com>
2022-12-14 11:42:47 +0100

    Update Resturant.js

--- screens/Resturant.js
+++ screens/Resturant.js
@@ -15,6 +15,7 @@ const Resturant = ({ route, navigation }) => {
   const scrollX = new Animated.Value(0)
   const [restaurant, setRestaurant] = useState(undefined);
   const [currentLocation, setCurrentLocation] = useState(undefined);
+  const [orderItems, setOrderItems] = useState([]);
 
   useEffect(() => {
     let { item, currentLocation } = route.params;
@@ -284,11 +285,106 @@ const Resturant = ({ route, navigation }) => {
   const renderOrder = () => {
     return (
       <View>
-        {
-          renderDots()
-        }
+          {
+              renderDots()
+          }
+          <View
+              style={{
+                  backgroundColor: COLORS.white,
+                  borderTopLeftRadius: 40,
+                  borderTopRightRadius: 40
+              }}
+          >
+              <View
+                  style={{
+                      flexDirection: 'row',
+                      justifyContent: 'space-between',
+                      paddingVertical: SIZES.padding * 2,
+                      paddingHorizontal: SIZES.padding * 3,
+                      borderBottomColor: COLORS.lightGray2,
+                      borderBottomWidth: 1
+                  }}
+              >
+                  {/* <Text style={{ ...FONTS.h3 }}>{getBasketItemCount()} items in Cart</Text>
+                  <Text style={{ ...FONTS.h3 }}>${sumOrder()}</Text> */}
+              </View>
+
+              <View
+                  style={{
+                      flexDirection: 'row',
+                      justifyContent: 'space-between',
+                      paddingVertical: SIZES.padding * 2,
+                      paddingHorizontal: SIZES.padding * 3
+                  }}
+              >
+                  <View style={{ flexDirection: 'row' }}>
+                      <Image
+                          source={icons.pin}
+                          resizeMode="contain"
+                          style={{
+                              width: 20,
+                              height: 20,
+                              tintColor: COLORS.darkgray
+                          }}
+                      />
+                      <Text style={{ marginLeft: SIZES.padding, ...FONTS.h4 }}>Location</Text>
+                  </View>
+
+                  <View style={{ flexDirection: 'row' }}>
+                      <Image
+                          source={icons.master_card}
+                          resizeMode="contain"
+                          style={{
+                              width: 20,
+                              height: 20,
+                              tintColor: COLORS.darkgray
+                          }}
+                      />
+                      <Text style={{ marginLeft: SIZES.padding, ...FONTS.h4 }}>8888</Text>
+                  </View>
+              </View>
+
+              {/* Order Button */}
+              <View
+                  style={{
+                      padding: SIZES.padding * 2,
+                      alignItems: 'center',
+                      justifyContent: 'center'
+                  }}
+              >
+                  <TouchableOpacity
+                      style={{
+                          width: SIZES.width * 0.9,
+                          padding: SIZES.padding,
+                          backgroundColor: COLORS.primary,
+                          alignItems: 'center',
+                          borderRadius: SIZES.radius
+                      }}
+                      onPress={() => navigation.navigate("OrderDelivery", {
+                          restaurant: restaurant,
+                          currentLocation: currentLocation
+                      })}
+                  >
+                      <Text style={{ color: COLORS.white, ...FONTS.h2 }}>Order</Text>
+                  </TouchableOpacity>
+              </View>
+          </View>
+
+          {isIphoneX() &&
+              <View
+                  style={{
+                      position: 'absolute',
+                      bottom: -34,
+                      left: 0,
+                      right: 0,
+                      height: 34,
+                      backgroundColor: COLORS.white
+                  }}
+              >
+              </View>
+          }
       </View>
-    )
+  )
   }
   return (
     <SafeAreaView style={styles.container}>
